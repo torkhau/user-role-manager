e156d80cd83a37001aae2bcaa6b4eed2a44c06c1
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-10-02 18:35:43 +0200

    feat: refactor ResponsiveDrawer and Dashboard components for improved layout and structure

--- frontend/src/components/layouts/ResponsiveDrawer/ResponsiveDrawer.tsx
+++ frontend/src/components/layouts/ResponsiveDrawer/ResponsiveDrawer.tsx
@@ -26,34 +26,46 @@ export function ResponsiveDrawer({ children }: { children?: ReactNode }) {
   return (
     <>
       <TopBar onMenuClick={handleDrawerToggle} />
-      <Box component='nav' sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }} aria-label='mailbox folders'>
-        <Drawer
-          variant='temporary'
-          open={mobileOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
+      <Box sx={{ width: '100vw', height: '100vh', display: 'flex' }}>
+        <Box component='nav' sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }} aria-label='mailbox folders'>
+          <Drawer
+            variant='temporary'
+            open={mobileOpen}
+            onTransitionEnd={handleDrawerTransitionEnd}
+            onClose={handleDrawerClose}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH },
+            }}
+            slotProps={{ root: { keepMounted: true } }}
+          >
+            <Sidebar />
+          </Drawer>
+          <Drawer
+            variant='permanent'
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH },
+            }}
+            open
+          >
+            <Sidebar />
+          </Drawer>
+        </Box>
+        <Box
+          component='main'
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH },
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+            height: '100vh',
+            overflow: 'hidden'
           }}
-          slotProps={{ root: { keepMounted: true } }}
         >
-          <Sidebar />
-        </Drawer>
-        <Drawer
-          variant='permanent'
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH },
-          }}
-          open
-        >
-          <Sidebar />
-        </Drawer>
-      </Box>
-      <Box component='main' sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` } }}>
-        <Toolbar />
-        {children}
+          <Toolbar />
+          {children}
+        </Box>
       </Box>
     </>
   );
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,27 +1,10 @@
-import { Box, Toolbar, Typography } from '@mui/material';
 import { ResponsiveDrawer } from '../../components/layouts';
-import { useMenuItemContext } from '../../core/contexts/menuItem';
+import { Main } from '../../components/layouts/Main';
 
 export function Dashboard() {
-  const { menuItem } = useMenuItemContext();
-
-  console.log(menuItem);
-
   return (
-    <Box sx={{ display: 'flex' }}>
-      <ResponsiveDrawer>
-        <Toolbar />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-          velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu
-          scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-          lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-          ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-        </Typography>
-      </ResponsiveDrawer>
-    </Box>
+    <ResponsiveDrawer>
+      <Main />
+    </ResponsiveDrawer>
   );
 }
