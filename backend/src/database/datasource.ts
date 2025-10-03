2ba8b0604f71960b9935afd08f7faa7b88268aa1
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-10-03 16:47:18 +0200

    feat: add migration support to appDatasource configuration

--- backend/src/database/datasource.ts
+++ backend/src/database/datasource.ts
@@ -1,4 +1,4 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './typeorm.config';
 
-export const appDatasource = new DataSource(dataSourceOptions);
+export const appDatasource = new DataSource({ ...dataSourceOptions, migrations: ['src/database/migrations/*.ts'] });
